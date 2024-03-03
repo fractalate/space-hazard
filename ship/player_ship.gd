be679e1992230077b23df69c655f16405b0cee08
Kate Novak <kje@cochleoid.com>
2024-03-03 12:49:19 -0500

    Neutralizing Project Gravity, Using Physics Process on PlayerShip

--- ship/player_ship.gd
+++ ship/player_ship.gd
@@ -5,8 +5,6 @@ const THRUST_ACCELERATION = 300
 
 @onready var Ship = $RigidBody2D
 
-
-
-func _process(delta):
+func _physics_process(delta):
 	var thrust = Input.get_vector('left', 'right', 'up', 'down')
 	Ship.linear_velocity += thrust * THRUST_ACCELERATION * delta
