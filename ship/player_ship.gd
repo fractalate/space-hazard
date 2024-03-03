444b8384cea330ebe59a6e8cf3313ff58af29465
Kate Novak <kje@cochleoid.com>
2024-03-03 12:46:01 -0500

    Prototype Controls for PlayerShip

--- ship/player_ship.gd
+++ ship/player_ship.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
+# pixels per second per second
+const THRUST_ACCELERATION = 300
+
+@onready var Ship = $RigidBody2D
+
+
+
 func _process(delta):
-	pass
+	var thrust = Input.get_vector('left', 'right', 'up', 'down')
+	Ship.linear_velocity += thrust * THRUST_ACCELERATION * delta
