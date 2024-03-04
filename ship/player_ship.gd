47ad29438d4367452a32c93a9c8efdfd526333f2
Kate Novak <kje@cochleoid.com>
2024-03-03 20:15:49 -0500

    Bottom Lights for PlayerShip

--- ship/player_ship.gd
+++ ship/player_ship.gd
@@ -9,6 +9,10 @@ const THRUST_ACCELERATION = 300
 @onready var TopThruster1Flame = $RigidBody2D/TopThruster1/Flame
 @onready var TopThruster2Flame = $RigidBody2D/TopThruster2/Flame
 @onready var BottomThrusterFlame = $RigidBody2D/BottomThruster/Flame
+@onready var BottomLightLight = $RigidBody2D/BottomLight/Light
+
+const BOTTOM_LIGHT_CYCLE_TIME: float = 2.0
+var bottom_light_time_passed: float = 0.0
 
 func _ready():
 	RearThrusterFlame.visible = false
@@ -16,6 +20,7 @@ func _ready():
 	TopThruster1Flame.visible = false
 	TopThruster2Flame.visible = false
 	BottomThrusterFlame.visible = false
+	BottomLightLight.visible = false
 
 func _physics_process(delta):
 	var thrust = Input.get_vector('left', 'right', 'up', 'down')
@@ -26,3 +31,12 @@ func _physics_process(delta):
 	TopThruster1Flame.visible = thrust.y > 0
 	TopThruster2Flame.visible = thrust.y > 0
 	BottomThrusterFlame.visible = thrust.y < 0
+
+	bottom_light_time_passed += delta
+	while bottom_light_time_passed >= BOTTOM_LIGHT_CYCLE_TIME:
+		bottom_light_time_passed -= BOTTOM_LIGHT_CYCLE_TIME
+	BottomLightLight.visible = (
+		bottom_light_time_passed >= .5 * BOTTOM_LIGHT_CYCLE_TIME and bottom_light_time_passed <= .6 * BOTTOM_LIGHT_CYCLE_TIME
+		or
+		bottom_light_time_passed >= .7 * BOTTOM_LIGHT_CYCLE_TIME and bottom_light_time_passed <= .8 * BOTTOM_LIGHT_CYCLE_TIME
+	)
