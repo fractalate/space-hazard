84d86d03a371bcaa38a867fa1c5e8eadf9f7b494
Kate Novak <kje@cochleoid.com>
2024-03-05 14:23:46 -0500

    Work-in-progress Buttons on Main Scene

--- main.gd
+++ main.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
+@onready var ButtonNewGame = $ButtonNewGame
+
 var LevelDemo = load('res://levels/demo/level_demo.tscn')
 var PlayerShip = load('res://ship/player_ship.tscn')
 
-func _ready():
+func start_level_demo():
 	add_child(LevelDemo.instantiate())
 	var playerShip = PlayerShip.instantiate()
 	playerShip.position = get_viewport_rect().size / 2
 	add_child(playerShip)
+
+func _ready():
+	ButtonNewGame.grab_focus()
