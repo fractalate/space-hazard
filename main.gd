c54fe9a0d2c475749637f4fd89a953264039a67f
Kate Novak <kje@cochleoid.com>
2024-03-13 07:38:05 -0400

    Renaming Demo Level to Test

--- main.gd
+++ main.gd
@@ -3,14 +3,14 @@ extends Node2D
 @onready var MainMenu = $MainMenu
 @onready var Background = $Background
 
-var LevelDemo = load('res://levels/demo/level_demo.tscn')
+var LevelTest = load('res://levels/test/level_test.tscn')
 var PlayerShip = load('res://ship/player_ship.tscn')
 
 var viewport_width_initial = ProjectSettings.get_setting("display/window/size/viewport_width")
 var viewport_height_initial = ProjectSettings.get_setting("display/window/size/viewport_height")
 
-func start_level_demo():
-	add_child(LevelDemo.instantiate())
+func start_level_test():
+	add_child(LevelTest.instantiate())
 	var playerShip = PlayerShip.instantiate()
 	playerShip.position = Vector2(viewport_width_initial, viewport_height_initial) / 2
 	add_child(playerShip)
@@ -24,6 +24,6 @@ func _ready():
 	var on_menu_closed = func():
 		set_main_menu_visible(false)
 	var on_start_marathon = func():
-		start_level_demo()
+		start_level_test()
 	MainMenu.connect('menu_closed', on_menu_closed)
 	MainMenu.connect('start_marathon', on_start_marathon)
