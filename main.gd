5485ff09b92e076e7439bb6661527ef615d1e0c1
Kate Novak <kje@cochleoid.com>
2024-03-13 20:01:36 -0400

    Starting Demo Level

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var MainMenu = $MainMenu
 @onready var Background = $Background
 
+var LevelDemo = load('res://levels/level_demo/level_demo.tscn')
 var LevelTest = load('res://levels/test/level_test.tscn')
 var PlayerShip = load('res://ship/player_ship.tscn')
 
@@ -11,6 +12,12 @@ var viewport_height_initial = ProjectSettings.get_setting("display/window/size/v
 
 func start_level_test():
 	add_child(LevelTest.instantiate())
+	var playerShip = PlayerShip.instantiate()
+	playerShip.position = Vector2(viewport_width_initial, viewport_height_initial) / 2
+	add_child(playerShip)
+
+func start_level_demo():
+	add_child(LevelDemo.instantiate())
 	var playerShip = PlayerShip.instantiate()
 	playerShip.position = Vector2(viewport_width_initial, viewport_height_initial) / 2
 	add_child(playerShip)
@@ -24,6 +31,7 @@ func _ready():
 	var on_menu_closed = func():
 		set_main_menu_visible(false)
 	var on_start_marathon = func():
-		start_level_test()
+		#start_level_test()
+		start_level_demo()
 	MainMenu.connect('menu_closed', on_menu_closed)
 	MainMenu.connect('start_marathon', on_start_marathon)
